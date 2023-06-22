f434091084ef8e32aefcf1e1882b04feb7aab6f3
TheChrisBot <chrisstrother@hotmail.com>
2023-06-22 14:52:01 -0700

    Adding funtionality and css to the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,61 @@
-// Write your Color component here
+import React, { useState } from 'react';
 
-const App = () => {
+const Color = ({ color, setSelectedColor, isSelected }) => {
+  const colorStyle = {
+    border: isSelected ? '5px solid black' : 'none',
+  };
+
   return (
+    <div
+      className={color}
+      style={colorStyle}
+      onClick={() => setSelectedColor(color)}
+    ></div>
+  );
+};
+
+const Picker = () => {
+  const [selectedColor, setSelectedColor] = useState('');
+return (
     <div id="container">
       <div id="navbar">
-        <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div>Pick A Color! </div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color
+          color="black"
+          setSelectedColor={setSelectedColor}
+          isSelected={selectedColor === 'black'}
+        />
+        <Color
+          color="red"
+          setSelectedColor={setSelectedColor}
+          isSelected={selectedColor === 'rede'}
+        />
+        <Color
+          color="white"
+          setSelectedColor={setSelectedColor}
+          isSelected={selectedColor === 'white'}
+        />
+        <Color
+          color="green"
+          setSelectedColor={setSelectedColor}
+          isSelected={selectedColor === 'green'}
+        />
+        <Color
+          color="blue"
+          setSelectedColor={setSelectedColor}
+          isSelected={selectedColor === 'blue'}
+        />
+        <Color
+          color="violet"
+          setSelectedColor={setSelectedColor}
+          isSelected={selectedColor === 'violet'}
+        />
+      </div>
     </div>
   );
 };
 
-export default App;
+export default Picker;
